4a9d8ce919f6464cdbdd9f2c0b4eb2a82f835cbd
ShkindI <128473050+ShkindI@users.noreply.github.com>
2023-03-23 19:12:32 +0300

    working...

--- add_client.py
+++ add_client.py
@@ -9,9 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from sucsessful_wndw import Ui_Sucsessful_windw
-from error_wndw import Ui_Error_windw
-from FUNCTION import add_new_client
+
 
 class Ui_Add_client(object):
     def setupUi(self, Add_client):
@@ -28,14 +26,11 @@ class Ui_Add_client(object):
         self.lineEdit_phone_num_client.setGeometry(QtCore.QRect(30, 230, 151, 22))
         self.lineEdit_phone_num_client.setReadOnly(False)
         self.lineEdit_phone_num_client.setObjectName("lineEdit_phone_num_client")
-        self.lineEdit_age_client = QtWidgets.QLineEdit(Add_client)
-        self.lineEdit_age_client.setGeometry(QtCore.QRect(30, 130, 151, 22))
-        self.lineEdit_age_client.setObjectName("lineEdit_age_client")
         self.label_name_client = QtWidgets.QLabel(Add_client)
         self.label_name_client.setGeometry(QtCore.QRect(34, 10, 191, 20))
         self.label_name_client.setObjectName("label_name_client")
         self.age_client = QtWidgets.QLabel(Add_client)
-        self.age_client.setGeometry(QtCore.QRect(30, 100, 141, 20))
+        self.age_client.setGeometry(QtCore.QRect(30, 100, 211, 20))
         self.age_client.setObjectName("age_client")
         self.Phone_number_client = QtWidgets.QLabel(Add_client)
         self.Phone_number_client.setGeometry(QtCore.QRect(30, 200, 171, 20))
@@ -44,14 +39,13 @@ class Ui_Add_client(object):
         self.label_error_name_client.setGeometry(QtCore.QRect(34, 70, 161, 20))
         self.label_error_name_client.setText("")
         self.label_error_name_client.setObjectName("label_error_name_client")
-        self.label_error_age_client = QtWidgets.QLabel(Add_client)
-        self.label_error_age_client.setGeometry(QtCore.QRect(34, 160, 161, 20))
-        self.label_error_age_client.setText("")
-        self.label_error_age_client.setObjectName("label_error_age_client")
         self.label_error_phone_num_client = QtWidgets.QLabel(Add_client)
         self.label_error_phone_num_client.setGeometry(QtCore.QRect(34, 260, 151, 20))
         self.label_error_phone_num_client.setText("")
         self.label_error_phone_num_client.setObjectName("label_error_phone_num_client")
+        self.dateEdit = QtWidgets.QDateEdit(Add_client)
+        self.dateEdit.setGeometry(QtCore.QRect(30, 140, 151, 22))
+        self.dateEdit.setObjectName("dateEdit")
 
         self.retranslateUi(Add_client)
         QtCore.QMetaObject.connectSlotsByName(Add_client)
@@ -61,83 +55,13 @@ class Ui_Add_client(object):
         Add_client.setWindowTitle(_translate("Add_client", "Add_client"))
         self.pushButton.setText(_translate("Add_client", "Подтвердить"))
         self.lineEdit_phone_num_client.setText(_translate("Add_client", "375"))
-        self.label_name_client.setText(_translate("Add_client", "Введите ФИО нового клиента:"))
-        self.age_client.setText(_translate("Add_client", "Укажите возраст:"))
+        self.label_name_client.setText(_translate("Add_client", "Введите имя нового клиента:"))
+        self.age_client.setText(_translate("Add_client", "Укажите дату рождения клиента:"))
         self.Phone_number_client.setText(_translate("Add_client", "Укажите номер телефона:"))
-
-        self.pushButton.clicked.connect(self.add_client)
-
-
-
-    def check_phone_num(self):
-        if self.lineEdit_phone_num_client.text() != '':
-            if self.lineEdit_phone_num_client.text() != '375':
-                if self.lineEdit_phone_num_client.text().isdigit():
-                    if len(self.lineEdit_phone_num_client.text()) == 12:
-                        return True
-                    else:
-                        self.label_error_phone_num_client.setText('Мало цифр для номера!')
-                        return False
-                else:
-                    self.label_error_phone_num_client.setText('Только цифры!!!')
-                    return False
-            else:
-                self.label_error_phone_num_client.setText('Вы ничего не ввели!')
-                return False
-        else:
-            self.label_error_phone_num_client.setText('Поле пустое!!!')
-            return False
-
-
-    def check_age(self):
-        if self.lineEdit_age_client.text() != '':
-            if self.lineEdit_age_client.text().isdigit():
-                if len(self.lineEdit_age_client.text()) >=3:
-                    self.label_error_age_client.setText('Больше 100-а лет?')
-                    return False
-                else:
-                    return True
-            else:
-                self.label_error_age_client.setText('Возраст указать цифрами!')
-                return False
-        else:
-            self.label_error_age_client.setText('Поле пустое!!!')
-            return False
-
-    def add_client(self):
-        name = any(x.isdigit() for x in self.lineEdit_name_client.text())
-        if name == False:
-            if self.check_phone_num() == True:
-                if self.check_age() == True:
-                    self.lineEdit_name_client.setReadOnly(True)
-                    self.lineEdit_age_client.setReadOnly(True)
-                    self.lineEdit_phone_num_client.setReadOnly(True)
-                    result = (
-                        self.lineEdit_name_client.text(), self.lineEdit_age_client.text(),
-                        self.lineEdit_phone_num_client.text())
-                    print(result)
-                    # add_new_client(result)
-                    answer = False
-                    if answer == True:
-                        sucsess = QtWidgets.QDialog()
-                        ui2 = Ui_Sucsessful_windw()
-                        ui2.setupUi(sucsess)
-                        sucsess.show()
-                        sucsess.exec_()
-                    else:
-                        error = QtWidgets.QDialog()
-                        ui2 = Ui_Error_windw()
-                        ui2.setupUi(error)
-                        error.show()
-                        error.exec_()
-        else:
-            self.label_error_name_client.setText('В имени не бывает цифр!')
-
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     Add_client = QtWidgets.QDialog()
     ui = Ui_Add_client()
